060e2fe281090164619ca16b893863e1680d5768
Deepak Singh <deepaksng00@Deepaks-MacBook-Pro.local>
2020-10-26 17:07:43 +0000

    Implementing semantic rule for method declarations

--- Decaf - Compiler/decaf-semantics.py
+++ Decaf - Compiler/decaf-semantics.py
@@ -19,26 +19,60 @@ class DecafSemanticChecker(DecafVisitor):
         data_type = ctx.data_type().getText()
         line_num = ctx.start.line
         
-        array_size = ctx.field_decl_args(0)
+        array_size = len(ctx.field_decl_args())
         
-        print(array_size.int_literal().getText())
-        # checking for multiple array declarations
-        for i in range(len(ctx.field_decl_args())):
+        array_size_decl = 1 # memory allocation for array declarations
+            
+        # checking for multiple array declarations and arrays of size 0 or less
+        for i in range(array_size):
             field_symbol = self.st.probe(ctx.field_decl_args(i).getText())
             if field_symbol != None:
-                print("Error on line", str(line_num) + ", field", "'" + str(ctx.field_decl_args(i).getText()) + "'", "already declared on line", field_symbol.line)
+                print("ERROR: On line", str(line_num) + ", field", "'" + ctx.field_decl_args(i).getText() + "'", "already declared on line", field_symbol.line)
+            elif ctx.field_decl_args(i).int_literal() != None:
+                if int(ctx.field_decl_args(i).int_literal().getText()) <= 0:
+                    array_size_decl = ctx.field_decl_args(i).int_literal().getText();
+                    print("ERROR: On line", str(line_num) + ", field", "'" + ctx.field_decl_args(i).getText() + "'.", "Cannot declare array of size", ctx.field_decl_args(i).int_literal().getText())
+                else:
+                    array_size_decl = 1;
             else:
                 field_symbol = VarSymbol(id=ctx.field_decl_args(i).getText(),
                                          type=data_type,
                                          line=line_num,
-                                         size=8,
+                                         size=8 * array_size_decl,
                                          mem=HEAP)
                 
                 self.st.addSymbol(field_symbol)
         
-        # checking for arrays of size 0
-        
         self.visitChildren(ctx)
+        
+    def visitMethod_decl(self, ctx:DecafParser.Method_declContext):
+        self.st.enterScope() # creating new scope for method
+        
+        method_args = len(ctx.method_decl_args())
+        line_num = ctx.start.line
+        
+        for i in range(method_args):
+            field_symbol = self.st.probe(ctx.method_decl_args(i).ID().getText())
+            if field_symbol != None:
+                print("ERROR: on line", str(line_num) + ", field '" + ctx.method_decl_args(i).ID().getText() + "'.", "Argument already declared for method '" + ctx.ID().getText() + "'")
+            else:
+                field_symbol = VarSymbol(id=ctx.method_decl_args(i).ID().getText(),
+                                         type=ctx.method_decl_args(i).data_type().getText(),
+                                         line=line_num,
+                                         size=8,
+                                         mem=HEAP)    
+                self.st.addSymbol(field_symbol)
+                
+        method_symbol = MethodSymbol(id=ctx.ID().getText(),
+                                     type=ctx.data_type().getText(),
+                                     line=line_num,
+                                     params=ctx.method_decl_args())
+        
+        self.self.addSymbol(method_symbol)
+            
+        self.visitChildren(ctx)
+        
+        self.st.exitScope()
 
 filein = open('test.dcf', 'r')
 lexer = DecafLexer(ant.InputStream(filein.read()))
